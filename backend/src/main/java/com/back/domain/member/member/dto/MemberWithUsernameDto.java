0604cedf87bae050715e361ae6f4cd2d56bda389
user.email <younsoo6103@naver.com>
2025-07-14 09:50:09 +0900

    step01

--- backend/src/main/java/com/back/domain/member/member/dto/MemberWithUsernameDto.java
+++ backend/src/main/java/com/back/domain/member/member/dto/MemberWithUsernameDto.java
@@ -1,15 +1,16 @@
 package com.back.domain.member.member.dto;
 
 import com.back.domain.member.member.entity.Member;
+import org.springframework.lang.NonNull;
 
 import java.time.LocalDateTime;
 
 public record MemberWithUsernameDto(
-        int id,
-        LocalDateTime createDate,
-        LocalDateTime modifyDate,
-        String name,
-        String username
+        @NonNull int id,
+        @NonNull LocalDateTime createDate,
+        @NonNull LocalDateTime modifyDate,
+        @NonNull String name,
+        @NonNull String username
 ) {
     public MemberWithUsernameDto(int id, LocalDateTime createDate, LocalDateTime modifyDate, String name, String username) {
         this.id = id;
